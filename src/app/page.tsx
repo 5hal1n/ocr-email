71a6a23cedcc3a89c5a449ba10027276390be0f9
Shalin Okada <shalin.okada@banso-corp.com>
2025-12-08 08:29:00 +0900

    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,12 @@ const Home = () => {
       <h1 className="text-2xl font-bold mb-4">領収書一覧</h1>
       <p className="mb-4">
         領収書は
-        <a href="mailto:banso@uldaebeloo.resend.app">
+        <a
+          target="_blank"
+          href="mailto:banso@uldaebeloo.resend.app?subject=領収書データの送付"
+          rel="noopener"
+          className="text-blue-600 underline ml-1 mr-1"
+        >
           banso@uldaebeloo.resend.app
         </a>
         に送信してください
