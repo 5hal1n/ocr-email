0cce11851b6fbb56a969d628ca62868434ac9e85
Shalin Okada <shalin.okada@banso-corp.com>
2025-12-08 07:58:08 +0900

    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { ReceiptDataTable } from "@/components/usecase/receiptDataTable";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black gap-12">
+    <main>
       <ReceiptDataTable />
     </main>
   );
