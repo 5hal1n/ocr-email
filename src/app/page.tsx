ea6a0ef221ae8042c98eb31d402409f790a057c7
Shalin Okada <shalin.okada@banso-corp.com>
2025-12-08 08:00:24 +0900

    ページコンポーネントにタイトルと説明を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { ReceiptDataTable } from "@/components/usecase/receiptDataTable";
 export default function Home() {
   return (
     <main>
+      <h1>領収書一覧</h1>
+      <p>領収書は{"<会社ID>@uldaebeloo.resend.app"}に送信してください</p>
       <ReceiptDataTable />
     </main>
   );
